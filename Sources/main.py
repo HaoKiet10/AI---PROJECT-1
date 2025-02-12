ef804dac6ef6372f2d6c2d3dd02b8663e4e36542
Lê Nhựt Duy <macos@192.168.0.103>
2025-02-12 23:49:01 +0700

    update interface

---
+++ Sources/main.py
@@ -0,0 +1,34 @@
+import DFS
+
+
+inputFileName = "../Input/Input-01.txt"
+outputFileName = inputFileName.replace("Input", "Output")
+
+def readFile():
+    with open(inputFileName, 'r') as file: 
+        line = file.readlines()
+
+    weightStone = list(map(int, line[0].strip().split()))  # get value and assign to list
+
+    array_1D = []
+    for line in line[1:]:
+        if line.strip():
+            array_1D.append(line.rstrip())
+
+    array = [list(ch) for ch in array_1D] # convert 1D to 2D
+    
+    with open(outputFileName, "w") as file:
+        pass
+    
+    return weightStone, array
+
+
+
+def main():
+    weightStone, array = readFile()
+    print(weightStone)
+    print(array)
+    
+    
+if __name__ == "__main__":
+    main()
