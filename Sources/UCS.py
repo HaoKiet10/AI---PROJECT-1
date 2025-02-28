0c3c9b0abdcfbd0807f5b6a680467379b3bfc41b
sinluvyue <sinluvyue@gmail.com>
2025-02-28 16:45:30 +0700

    bla bla bla

--- Sources/UCS.py
+++ Sources/UCS.py
@@ -13,18 +13,6 @@ import time
 import tracemalloc
 import supportFile as spf
 
-class DualPriorityQueue(PriorityQueue):
-    def __init__(self, maxPQ=False):
-        PriorityQueue.__init__(self)
-        self.reverse = -1 if maxPQ else 1
-
-    def put(self, priority, data):
-        PriorityQueue.put(self, (self.reverse * priority, data))
-
-    def get(self, *args, **kwargs):
-        priority, data = PriorityQueue.get(self, *args, **kwargs)
-        return self.reverse * priority, data
-
 def output(filePath, cur_map, time, memory):
     traceback(cur_map)
     with open(filePath, "a") as f:
@@ -40,6 +28,15 @@ def traceback(cur_map):
     print(cur_map.stonePos) 
     print(cur_map.weightPush)
 
+def ending(startTime, filePath, cur_map):
+    timeUsed = (time.time() - startTime) * 1000
+    timeUsed = int(timeUsed)
+    currentMemory, peakMemory = tracemalloc.get_traced_memory() 
+    tracemalloc.stop()
+    memoryUsed = round((peakMemory / (1024 * 1024)), 3)
+    output(filePath, cur_map, timeUsed, memoryUsed)
+    return cur_map
+
 
 def UCS(board, weightedStone, filePath):
     startTime = time.time()
@@ -52,51 +49,34 @@ def UCS(board, weightedStone, filePath):
 
 
     visited_map = []
-    list_map = DualPriorityQueue(maxPQ=True)
-    list_map.put(start_map.weightPush, start_map)
+    list_map = PriorityQueue()
+    list_map.put(start_map)
     node = 0
 
     # j = 410
     # while j > 0:
     while not list_map.empty():
-        print(node)
         node += 1
-        priority, cur_map = list_map.get()
+        cur_map = list_map.get()
+        ares = spf.findPosAres(cur_map.board)
         cur_map.node = node
         visited_map.append(cur_map)
 
 
         if spf.checkWinner(cur_map.board, switchList):
-<<<<<<< Updated upstream
-            timeUsed = (time.time() - startTime) * 1000
-            timeUsed = int(timeUsed)
-            currentMemory, peakMemory = tracemalloc.get_traced_memory() 
-            tracemalloc.stop()
-            memoryUsed = round((peakMemory / (1024 * 1024)), 3)
-            output(filePath, cur_map, timeUsed, memoryUsed)
-            return cur_map
-=======
             return ending(startTime, filePath, cur_map)
->>>>>>> Stashed changes
 
-        next_direction = spf.nextDirections(cur_map.board, spf.findPosAres(cur_map.board))
+        next_direction = spf.nextDirections(cur_map.board, ares)
         for i in next_direction:
-            new_board, newStonePos = spf.move(cur_map.board, cur_map.stonePos, i, spf.findPosAres(cur_map.board), switchList)
+            new_board, newStonePos = spf.move(cur_map.board, cur_map.stonePos, i, ares, switchList)
             if not spf.checkSameBoard(new_board, visited_map):
-                path = cur_map.path + spf.moveDirection(cur_map.board, i, spf.findPosAres(cur_map.board))
+                path = cur_map.path + spf.moveDirection(cur_map.board, i, ares)
                 weight = cur_map.weightPush + spf.checkWeight(cur_map.board, cur_map.stonePos, i)
                 new_map = spf.state(new_board, cur_map, path, weight, node, newStonePos)
-                list_map.put(new_map.weightPush, new_map)
+                list_map.put(new_map)
 
             endTime = time.time()
             if endTime - startTime > spf.TIMEOUT:
-<<<<<<< Updated upstream
-                return None
-        # j-=1
-    print("Not found")
-    return None
-=======
                 return ending(startTime, filePath, None)
         # j-=1
     return ending(startTime, filePath, None)
->>>>>>> Stashed changes
--- Sources/main.py
+++ Sources/main.py
@@ -2,9 +2,6 @@ import supportFile as spf
 import BFS
 import DFS
 import UCS
-<<<<<<< Updated upstream
-
-=======
 import DFS
 import Swarm
 
@@ -15,7 +12,6 @@ import Swarm
 # import home as ui
 
 
->>>>>>> Stashed changes
 inputFileName = "Input/Input-01.txt"
 outputFileName = inputFileName.replace("Input", "Output")
 
@@ -40,16 +36,9 @@ def readFile():
 
 
 def main():
-<<<<<<< Updated upstream
-    weightStone, array = readFile()
-    # bfsState = BFS.BFS(outputFileName, array, weightStone)
-    ucs = UCS.UCS(array, weightStone, outputFileName)
-    # dfs = DFS.DFS(outputFileName, array, weightStone)
-    
-=======
     weightStone, board = readFile()
     # bfsState = BFS.BFS(outputFileName, board, weightStone)
-    # ucs = UCS.UCS(board, weightStone, outputFileName)
+    ucs = UCS.UCS(board, weightStone, outputFileName)
     # root = tk.Tk()
     # app = Sokoban(root)
     # root.mainloop() 
@@ -57,7 +46,6 @@ def main():
     # dfs = DFS.DFS(outputFileName, board, weightStone)
     # ucs = UCS.UCS(board, weightStone, outputFileName)
     # dfs = DFS.DFS(outputFileName, board, weightStone)
-    swarm = Swarm.ACO(outputFileName, board, weightStone)
->>>>>>> Stashed changes
+    # swarm = Swarm.ACO(outputFileName, board, weightStone)
 if __name__ == "__main__":
     main()
